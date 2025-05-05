c9a6a7f1de523f19b8c3eb4de8b5ce226d550867
mfaiz23 <mohfaiz083gmail>
2025-05-06 00:09:09 +0700

    Commit project tanpa node_modules

--- app/Views/templates/main.php
+++ app/Views/templates/main.php
@@ -1,32 +1,34 @@
+
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-    />
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link href="<?= base_url('./public/assets/css/style.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/main.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/fullcalendar.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/lineicons.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/materialdesignicons.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/morris.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/loading-bar.min.css') ?>" rel="stylesheet">
     <title>Remind Planner</title>
   </head>
   <body
-    x-data="{ page: 'comingSoon', 'loaded': true, 'darkMode': false, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
-    x-init="
-         darkMode = JSON.parse(localStorage.getItem('darkMode'));
-         $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
-    :class="{'dark bg-gray-900': darkMode === true}"
   >
-    <!-- ===== Preloader Start ===== -->
-    <include src="./public/assets/partials/preloader.html"></include>
+    
 
-<body>
-        
+    <?= $this->renderSection('login') ?>
 
-    <?= $this->renderSection('content') ?>
+    <script src="<?= base_url('assets/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/main.js') ?>"></script>
+    <script src="<?= base_url('assets/js/fullcalendar.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jvectormap.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/loading-bar.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/moment.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/polyfill.js') ?>"></script>
+    <script src="<?= base_url('assets/js/world-merc.js') ?>"></script>
 
-    <script src="<?= base_url('./public/assets/js/index.js') ?>"></script>
-    <?= $this->renderSection('scripts') ?>
-
-</body>
+  </body>
 </html>
---
+++ app/Views/templates/navbar.php
@@ -0,0 +1,1 @@
+<? $this->extends ('main'); ?>
